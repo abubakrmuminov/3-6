d69745dbb43594dcc74b84d91baa46bc94fcb46a
Abubakr <170409v@gmail.com>
2025-04-10 16:45:10 +0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+/*=================*/
+/*Amaliy mashg'ulot*/
+/*=================*/
+
 const num = prompt("sonni kiriting");
 const first = +num[0];
 const second = +num[1];
@@ -134,3 +138,204 @@ switch (third) {
 }
 
 console.log(thirdnum, secondnum, firstnum);
+
+/*=======*/
+/*1-mashq*/
+/*=======*/
+
+const day = +prompt("son kiriting");
+
+switch (day) {
+  case 1:
+    console.log("Dushanba");
+    break;
+
+  case 2:
+    console.log("Seshanba");
+    break;
+
+  case 3:
+    console.log("Chorshanba");
+    break;
+
+  case 4:
+    console.log("Payshanba");
+    break;
+
+  case 5:
+    !!!!!!!console.log("Juma");
+    break;
+
+  case 6:
+    console.log("Shanba");
+    break;
+
+  case 7:
+    console.log("Yakshanba");
+    break;
+
+  default:
+    console.log("1dan 7gacha bo'lgan sonni kiriting");
+    break;
+}
+
+/*2-mashq*/
+
+const baho = prompt("bahoni kiriting");
+
+switch (true) {
+  case +baho < 50:
+    console.log("yomon");
+    break;
+
+  case +baho < 90:
+    console.log("o'rtacha");
+    break;
+
+  case +baho >= 90:
+    console.log("yaxshi");
+    break;
+}
+
+/*=======*/
+/*3-mashq*/
+/*=======*/
+
+const age = prompt("yoshingiz nechchida");
+let agenum = "";
+
+if (age >= 18 && age < 30) {
+  agenum = "salom, yoshlar!";
+} else if (age > 30) {
+  agenum = "salom, katta odam!";
+} else {
+  agenum = "kirish taqiqlanadi";
+}
+
+console.log(agenum);
+
+/*=======*/
+/*4-mashq*/
+/*=======*/
+
+const kun = +prompt("son kiriting");
+
+switch (kun) {
+  case 1:
+    console.log("Dushanba");
+    break;
+
+  case 2:
+    console.log("Seshanba");
+    break;
+
+  case 3:
+    console.log("Chorshanba");
+    break;
+
+  case 4:
+    console.log("Payshanba");
+    break;
+
+  case 5:
+    console.log("Juma");
+    break;
+
+  case 6:
+    console.log("Shanba");
+    break;
+
+  case 7:
+    console.log("Yakshanba");
+    break;
+
+  default:
+    console.log("1dan 7gacha bo'lgan sonni kiriting");
+    break;
+}
+
+/*=======*/
+/*5-mashq*/
+/*=======*/
+
+const grade = prompt("ballni kiriting");
+
+if (grade >= 90 && grade <= 100) console.log("A");
+else if (grade >= 75 && grade < 90) console.log("B");
+else if (grade >= 50 && grade < 75) console.log("C");
+else if (grade >= 0 && grade < 50) console.log("D");
+else console.log("1 dan 100 gacha bo'lgan son kiriting");
+
+/*=======*/
+/*6-mashq*/
+/*=======*/
+
+const raqam = prompt("raqam kiriting");
+
+if (raqam < 0) console.log("bu manfiy son");
+else if (raqam > 0) console.log("bu musbat son");
+else if (raqam == 0) console.log("bu nol");
+else console.log("bu raqam emas");
+
+/*=======*/
+/*7-mashq*/
+/*=======*/
+
+const dam = +prompt("1 dan 7 gacha son kiriting");
+
+switch (dam) {
+  case 6:
+    console.log("dam olish kuni");
+    break;
+  case 7:
+    console.log("dam olish kuni");
+    break;
+  case 1:
+    console.log("ish kuni");
+    break;
+  case 2:
+    console.log("ish kuni");
+    break;
+  case 3:
+    console.log("ish kuni");
+    break;
+  case 4:
+    console.log("ish kuni");
+    break;
+  case 5:
+    console.log("ish kuni");
+    break;
+  default:
+    console.log("1dan 7gacha son kiriting");
+    break;
+}
+
+/*=======*/
+/*8-mashq/*
+/*=======*/
+
+const score = prompt("ballni kiriting");
+
+if (score >= 90 && grade <= 100) console.log("A");
+else if (score >= 75 && grade < 90) console.log("B");
+else if (score >= 50 && grade < 75) console.log("C");
+else if (score >= 0 && grade < 50) console.log("D");
+else console.log("1 dan 100 gacha bo'lgan son kiriting");
+
+/*=======*/
+/*9-mashq*/
+/*=======*/
+
+const password = prompt("parolni kiriting");
+
+if (password == 1234) console.log("parol to'gri");
+else console.log("parol noto'g'ri");
+
+/*========*/
+/*10-mashq*/
+/*========*/
+
+const yosh = prompt("yoshingiz nechchida");
+
+if (yosh >= 18) console.log("ro'yxattan o'ta olasiz");
+else console.log("ro'yxattan o'ta ololmaysiz");
